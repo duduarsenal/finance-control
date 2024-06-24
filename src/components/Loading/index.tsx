4b79b7a925b9eb9f96da012b4957a3c5335022c6
Eduardo Souza <eduardo.souza@hexagon.pro>
2024-06-24 11:34:22 -0300

    fix: hot fix image loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import { cn, Icons } from "@utils";
 
 export function Loading({isTrue = false, className, message}: LoadingProps){
     return (
-        <div className={cn("fixed w-screen h-screen z-[9999] bg-[url('src/assets/bg-gradient.png')] flex flex-col items-center justify-center text-brand-white", 
+        <div className={cn("loading fixed w-screen h-screen z-[9999] flex flex-col items-center justify-center text-brand-white", 
             className, 
             {"invisible": !isTrue}
         )}
