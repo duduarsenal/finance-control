4e7fe194efb1c9d1c5cee7de6cb99a47902b81e6
Eduardo Souza <eduardo.souza@hexagon.pro>
2024-06-24 11:19:38 -0300

    fix: ajuste do import imagem

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,6 +4,7 @@ import { IsErrorProps } from "@typings";
 import { cn } from "@utils";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import bgGradient from 'src/assets/bg-gradient.png'
 
 export default function Layout({error}: IsErrorProps) {
 
@@ -18,7 +19,7 @@ export default function Layout({error}: IsErrorProps) {
       <Loading isTrue={isLoading} />
       <NotifyManager notifications={notifications} removeNotification={removeNotification} />
       <div className={cn("px-28 text-brand-white", { "pt-0": isPageHeader == "/login"})}>
-        <img src="src/assets/bg-gradient.png" 
+        <img src={bgGradient}
           alt="Background gradient" 
           className="fixed top-0 left-0 w-screen h-screen bg-black object-fit -z-10" 
         />
